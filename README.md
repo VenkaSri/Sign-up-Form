886904ddd0c1101320d1df0b95396e140ae035ff
VenkaSri <venkasritharan@gmail.com>
2022-03-01 22:54:56 -0500

    html & css complete, updated README

---
+++ README.md
@@ -0,0 +1,12 @@
+# SIGN-UP FORM
+
+A simple form page writted in HTML and CSS. I did it based on design file provided by [The Odin Project curriculum.](https://www.theodinproject.com/paths/full-stack-javascript/courses/intermediate-html-and-css/lessons/sign-up-form)
+
+![Design image](https://imgur.com/a/7lsSOuz)
+
+[Click here for a live preview](https://venkasri.github.io/Sign-up-Form/)
+
+
+### Tools
+- Visual Studio Code
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,39 @@
-const fields = document.querySelectorAll('form > div > div');
+const fields = document.querySelectorAll("input");
+const lbl = document.querySelectorAll("label");
+const pass = document.querySelector("#pass");
+const cPass = document.querySelector("#cPass");
+const subBtn = document.querySelector(".btn");
+const myForm = document.querySelector('#myForm');
 
+fields.forEach((item) => {
+  lbl.forEach((i) => {
+    item.addEventListener(
+      "focus",
+      (e) => {
+        if (i.getAttribute("for") === e.target.id) {
 
-fields.forEach(item => {
-    item.addEventListener('focus', (e) => {
-        
-    }, true)
+          i.classList.add("labelName");
+          e.target.placeholder = "";
+        }
+      },
+      true
+    );
+  });
+});
+
+subBtn.addEventListener('click', () => {
+  // if(cPass.value !== pass.value) {
+  //   cPass.classList.add('error');
+  //   pass.classList.add('error');
+  // } else {
+
+  // }
+  document.theForm.submit();
+  alert('hello');
 })
+
+
+
+function myFunction() {
+  
+}
