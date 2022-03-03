33498804dc320cab6eaea04a6ca1700f2b21dfdd
VenkaSri <venkasritharan@gmail.com>
2022-03-02 19:47:42 -0500

    added favicon, completed validation, updated README

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # SIGN-UP FORM
 
-A simple form page writted in HTML and CSS. I did it based on design file provided by [The Odin Project curriculum.](https://www.theodinproject.com/paths/full-stack-javascript/courses/intermediate-html-and-css/lessons/sign-up-form)
-
-![Design image](https://imgur.com/a/7lsSOuz)
+A simple form page based on design file provided by [The Odin Project curriculum.](https://www.theodinproject.com/paths/full-stack-javascript/courses/intermediate-html-and-css/lessons/sign-up-form)
 
 [Click here for a live preview](https://venkasri.github.io/Sign-up-Form/)
 
+### Design file image
+![Design file image](https://i.imgur.com/RrskYfo.png)
+
+
+### Demo
+![DEMO](https://i.imgur.com/gSNR46I.gif)
+
+
+
+### Features added
+- Empty fields validation 
+- Password validation 
+- Media queries 
 
 ### Tools
 - Visual Studio Code
--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ const lbl = document.querySelectorAll("label");
 const pass = document.querySelector("#pass");
 const cPass = document.querySelector("#cPass");
 const subBtn = document.querySelector(".btn");
-const myForm = document.querySelector('#myForm');
+const myForm = document.querySelector("#myForm");
+const errMsg = document.querySelector(".erMsg");
 
 fields.forEach((item) => {
   lbl.forEach((i) => {
@@ -11,7 +12,6 @@ fields.forEach((item) => {
       "focus",
       (e) => {
         if (i.getAttribute("for") === e.target.id) {
-
           i.classList.add("labelName");
           e.target.placeholder = "";
         }
@@ -21,19 +21,37 @@ fields.forEach((item) => {
   });
 });
 
-subBtn.addEventListener('click', () => {
-  // if(cPass.value !== pass.value) {
-  //   cPass.classList.add('error');
-  //   pass.classList.add('error');
-  // } else {
+myForm.addEventListener("submit", (e) => {
+  fields.forEach((item) => {
+    if (isEmpty(item) === false) {
+      errMsg.innerHTML = "Empty field(s)";
+      item.classList.add("error");
+      e.preventDefault();
+    } else {
+      item.classList.remove("error");
+      if (password() === false) {
+        errMsg.innerHTML = "Passwords don't match";
+        pass.classList.add("error");
+        cPass.classList.add("error");
+        e.preventDefault();
+      } else {
+        pass.classList.remove("error");
+        cPass.classList.remove("error");
+      }
+    }
+  });
+});
 
-  // }
-  document.theForm.submit();
-  alert('hello');
-})
+function isEmpty(inType) {
+  if (inType.value === "" || inType.value == null) {
+    return false;
+  }
+  return true;
+}
 
-
-
-function myFunction() {
-  
+function password() {
+  if (pass.value !== cPass.value) {
+    return false;
+  }
+  return true;
 }
